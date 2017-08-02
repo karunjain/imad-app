c10353d30fae59aebe9d87195ac05f4cf896c1de
karunjain <karun.jain94@gmail.com>
2017-08-03 01:14:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/SubTopics', function (req, res) {
 res.send('Sub Topics one requested and will be served here');
     
 });
-app.get('/ SubsTopics', function (req, res) {
+app.get('/SubsTopics', function (req, res) {
 res.send('Sub-Sub Topics one requested and will be served here');
     
 });
