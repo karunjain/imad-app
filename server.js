791bf6fb0d9c8d9426a3484786baa5a9d9082810
karunjain <karun.jain94@gmail.com>
2017-08-03 01:35:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ res.send('Spiderman.....!!!!');
     
 });
 
-app.get('/google', function (req, res) {
+app.get('/google.html', function (req, res) {
 res.send( link ="www.google.co.in");
     
 });
