c5f5dd13ed990a12492a42efef94570ecd09a31c
karunjain <karun.jain94@gmail.com>
2017-08-03 17:27:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,7 @@ element.innerHTML='Welcome Aboard..?';
 var img =document.getElementById('k');
 img.onclick=function()
 {
-    img.style.marginLeft='100px';
+   
     img.style.marginRight='100px';
-    img.style.marginLeft='100px';
-    img.style.marginRight='100px';
-    img.style.marginLeft='100px';
+ 
 }
