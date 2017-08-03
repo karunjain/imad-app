d4e52823653c44c551349059432ee23127ac323e
karunjain <karun.jain94@gmail.com>
2017-08-03 17:15:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element =document.getElementID('main');
 element.innerHTML='new value';
+
+var img =document.getElementID('img');
+img.onclick=function()
+{
+    img.style.marginLeft='100px';
+}
